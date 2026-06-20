d0408a294184e6661f41e880cdaa636c27485418
Pratik Das <daspratik500@gmail.com>
2026-06-20 13:52:19 +0200

    Initial commit

---
+++ app/routes.ts
@@ -0,0 +1,4 @@
+//It is a react router configuration file, by default it has only one home page react routing path
+import { type RouteConfig, index } from "@react-router/dev/routes";
+
+export default [index("routes/home.tsx")] satisfies RouteConfig;
