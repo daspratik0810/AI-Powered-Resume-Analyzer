fa524c8b79132a034b7e85b971a7c171cd9a9beb
Pratik Das <daspratik0810@gmail.com>
2026-07-01 18:21:16 +0000

    fetch real data and resume on homepage

--- app/components/ResumeCards.tsx
+++ app/components/ResumeCards.tsx
@@ -1,19 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from "react-router";
 import ScoreCircle from "~/components/ScoreCircle";
+import resume from '~/routes/resume';
+import {useEffect} from "react";
+import { usePuterStore } from '~/lib/puter';
 
 
 const ResumeCards = ({resume:{id,companyName,jobTitle, feedback, imagePath}}:{resume : Resume}) => {
     //now because we used element destructuring and "Resume" from index.d.ts. It knows that there is a const resume, which has an id with datatypes string
     //the section below has many reusuable resume cards which also navigates using react router to the specific resume.id
+    const {fs} = usePuterStore()
+    const [resumeUrl,setResumeUrl] = useState("")
+    
+    useEffect( () => {
+    const loadResume = async () => {
+       const blob = await fs.read(imagePath)
+       if(!blob) return
+       let url = URL.createObjectURL(blob)
+       setResumeUrl(url)
+    }
+    loadResume()
+  }, [imagePath])
+    
+    
+    
     return (
     <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000" >
 
         {/*RESUME-CARD-HEADER*/}
         <div className="resume-card-header">
             <div className="flex flex-col gap-2">
-                <h2 className="!text-black font-bold break-words">{companyName}</h2>
-                <h3 className="text-gray-600 text-lg break-words">{jobTitle}</h3>
+                {companyName && <h2 className="!text-black font-bold break-words">{companyName}</h2>}
+               {jobTitle && <h3 className="text-gray-600 text-lg break-words">{jobTitle}</h3>} 
+               {!companyName && !jobTitle && <h2 className="text-gray-600 text-lg break-words">Resume</h2>}
             </div>
 
             {/*below section we can do it with SVG file, but as we need info inside so we are making with react components*/}
@@ -23,14 +42,16 @@ const ResumeCards = ({resume:{id,companyName,jobTitle, feedback, imagePath}}:{re
         </div>
 
         {/*RESUME-CARD-IMAGE*/}
+        {resumeUrl && (
         <div className="gradient-border animate-in fade-in duration-1000">
             <div className="w-full h-full">
-                <img src={imagePath}
+                <img src={resumeUrl}
                 alt="resume"
                  className="w-full h-[420px] max-sm:h-[280px] object-contain"
                 />
             </div>
         </div>
+      )}
     </Link>
   )
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,9 @@
 import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
-import {resumes} from "~/constants";
 import ResumeCards from "~/components/ResumeCards";
 import {usePuterStore} from "~/lib/puter";
-import {useLocation, useNavigate} from "react-router";
-import {useEffect} from "react";
+import {Link, useNavigate} from "react-router";
+import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -16,14 +15,36 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
 
   //it is a custom hook created in puter.js, so we can simply import and use it
-  const {isLoading, auth} = usePuterStore();
+  const {isLoading, auth, fs, kv} = usePuterStore();
   const navigate = useNavigate()
+  const [resumeUrl,setResumeUrl] = useState("")
+  const [resumes,setResumes] = useState<Resume[]>([])
+  const [loadingResumes,setLoadingResumes] = useState(false)
+
+
 
   //useEffect is a react hook that runs after the component has been rendered and dependencies is changed.
   //when a user tries to access a secure route but they are not authenticated, then user is re-directed to auth again,that means they are blocked at auth. But if the user is authenticated then it will directed/navigated to the next page where they want to be directed
   useEffect( () =>{
     if(!isLoading && !auth.isAuthenticated) navigate('/auth?next=/')
   }, [isLoading, auth.isAuthenticated, navigate])
+  
+  useEffect( () =>{
+    const loadResumes = async () => {
+      setLoadingResumes(true)
+      const resumes = ( await kv.list("resume:*", true)) as KVItem[]
+      
+      const parsedResumes = resumes?.map( (resume) => (
+        JSON.parse(resume.value) as Resume
+      ))
+
+      console.log("parsedResumes", parsedResumes)
+      setResumes(parsedResumes || [])
+      setLoadingResumes(false)
+    }
+  
+    loadResumes()
+  }, [])
 
 
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
@@ -35,14 +56,33 @@ export default function Home() {
     <section className="main-section">
       <div className="page-heading py-16">
         <h1>Your Resume Has a Story. We Reveal It.</h1>
-        <h2> Every resume has blind spots. CVorithm finds them, explains them, and helps you fix them before recruiters
+        {!loadingResumes && resumes?.length === 0 ? (
+          <h2>Upload your first resume to get started. CVorithm will analyze it and provide detailed feedback to help you improve.</h2>
+        ): (
+          <h2> Every resume has blind spots. Review your submissions and CVorithm finds them, explains them and helps you fix them before recruiters
           ever notice.</h2>
+        )}
       </div>
+    
+      {loadingResumes && (
+        <div className="flex flex-col items-center justify-center">
+          <img src="/images/resume-scan-2.gif" className="w-16 h-16 animate-spin"/>
+        </div>
+      )}
+
+      {!loadingResumes && resumes?.length === 0 && (
+        <div className="flex flex-col items-center justify-centermt-10 gap-4">
+          <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+            Upload Resume
+          </Link>
+        </div>
+      )}
+      
 
       {/*TEMPLETES*/}
       {/* "resumes" from index.ts are an array of objects that contains different resumes*/}
 
-      {resumes.length > 0 && (
+      {!loadingResumes && resumes.length > 0 && (
           <div className="resumes-section">
             {resumes.map((resume) => (
               <ResumeCards key={resume.id} resume={resume} />
