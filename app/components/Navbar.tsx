32aa5e5ede64963f4caeaf098a7288cc654c2361
Pratik Das <daspratik0810@gmail.com>
2026-07-06 21:41:16 +0000

    logout button with full functionality

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,8 +1,20 @@
 //it is a functional component of react
 import React from 'react'
-import {Link} from "react-router";
+import {Link, useNavigate} from "react-router";
+import {usePuterStore} from "~/lib/puter";
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const { auth } = usePuterStore();
+
+  const handleLogout = async () => {
+    try {
+      await auth.signOut();
+    } finally {
+      navigate('/auth');
+    }
+  };
+
   return (
     <nav className="navbar">
         {/*it is coming from react-router, clicking "CVortithm" will lead to home directory*/}
@@ -10,11 +22,22 @@ const Navbar = () => {
             <p className="text-2xl font-bold text-gradient">CVorithm</p>
         </Link>
 
-        {/*it is coming from react-router, clicking "Upload Resume" will lead to upload directory*/}
-        <Link to="/upload" className="primary-button w-fit">
-            Upload Resume
-        </Link>
+        <div className="flex items-center gap-3">
+          {/*it is coming from react-router, clicking "Upload Resume" will lead to upload directory*/}
+          <Link to="/upload" className="primary-button w-fit">
+              Upload Resume
+          </Link>
 
+          {auth.isAuthenticated && (
+            <button
+              type="button"
+              onClick={handleLogout}
+              className="logout-button"
+            >
+              Logout
+            </button>
+          )}
+        </div>
     </nav>
   )
 }
